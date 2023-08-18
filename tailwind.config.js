3b1daf3f64817ebb2ca3fe42e18d9aec7d161406
Ale Onta <casualOTN@gmail.com>
2023-08-18 11:49:12 +0200

    adds tailwind -global- variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      xs: "536px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     extend: {
       colors: {
         "main-gray": "#455262",
         "blueish-gray": "#404952",
+        "smooth-gray": "rgba(211, 211, 211, 0.13)",
+        "absolute-gray": "#484869d8",
       },
       fontFamily: {
         titles: ["Inter", "sans-serif"],
         body: ["Roboto Flex", "sans-serif"],
+        custom: ["Karla", "sans-serif"],
       },
     },
   },
