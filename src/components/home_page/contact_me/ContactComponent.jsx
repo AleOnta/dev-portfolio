5a15fc1e44b6028783763a6c37a8005c5c20aa29
Ale Onta <casualOTN@gmail.com>
2023-08-30 21:47:22 +0200

    fixed sections titles

--- src/components/home_page/contact_me/ContactComponent.jsx
+++ src/components/home_page/contact_me/ContactComponent.jsx
@@ -5,7 +5,7 @@ import ContactFormComponent from "./ContactFormComponent";
 
 export default function ContactComponent() {
   return (
-    <motion.div className="container px-6 mt-10 mb-28 md:mt-32 pb-0 md:p-0 md:px-8">
+    <motion.div className="container px-6 mt-10 mb-28 md:mt-32 pb-0 md:p-0 md:px-8 pt-6">
       <motion.div className="mb-16 md:mb-20 lg:mb-24">
         <motion.h2 className="font-titles text-4xl md:text-5xl lg:text-6xl text-white md:px-12 md:py-8 mb-6 drop-shadow-xl text-center font-normal uppercase tracking-widest">
           My Contacts
--- src/components/home_page/my_projects/ProjectTopDescription.jsx
+++ src/components/home_page/my_projects/ProjectTopDescription.jsx
@@ -4,16 +4,16 @@ import { motion } from "framer-motion";
 export default function ProjectTopDescription() {
   return (
     <motion.div className="mt-10 md:mt-32" id="project-top-description">
-      <h2 className="font-titles font-bold text-4xl md:text-5xl text-white tracking-tight md:px-12 md:py-8 mb-6 drop-shadow-xl">
+      <h2 className="font-titles text-4xl md:text-5xl lg:text-6xl text-white md:px-12 md:py-8 mb-6 drop-shadow-xl text-center font-normal uppercase tracking-widest">
         My Projects
       </h2>
-      <motion.div className="p-4 bg-gun-metal bg-opacity-50 md:mx-12 mb-5 md:mb-12 rounded-lg shadow-lg">
-        <motion.p className="text-sm md:text-xl font-thin text-snow-white">
+      <motion.div className="p-4 md:mx-12 mb-5 md:mb-12 text-center">
+        <motion.p className="text-xs md:text-sm font-thin text-snow-white">
           In this section, there are only some of my projects, as listing them
           would be too long and meaningless. If you are interested in seeing
           someone more, visit my GitHub profile.
         </motion.p>
-        <motion.p className="text-sm md:text-xl font-thin text-snow-white">
+        <motion.p className="text-xs md:text-sm font-thin text-snow-white">
           (Please note that a good number of repositories, especially the least
           recent, represent the daily exercises carried out during the Epicode
           Bootcamp, and are therefore not real projects)
