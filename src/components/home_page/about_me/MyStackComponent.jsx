e618e1c3d1c2d95842a31d49d18190e4fb2bbe39
Ale Onta <casualOTN@gmail.com>
2023-08-30 19:25:16 +0200

    create new component for svg to add into about left component

--- src/components/home_page/about_me/MyStackComponent.jsx
+++ src/components/home_page/about_me/MyStackComponent.jsx
@@ -92,168 +92,166 @@ export default function MyStackComponent() {
   }, []);
 
   return (
-    <div className="w-full">
-      <h5 className=" w-full font-titles font-bold text-2xl xs:mb-5 xs:text-3xl md:text-3xl text-white mb-2 md:mb-12  lg:mb-10 tracking-tight">
-        What's my Stack?
+    <div className="w-full flex items-center justify-between relative">
+      <h5 className="font-titles font-bold text-4xl xs:text-5xl md:text-3xl absolute -top-8 left-0 text-white tracking-tight drop-shadow-xl">
+        My Stack
       </h5>
-      <div className="w-full flex items-center justify-between">
-        <div className="stack-vector-container ms-8">
-          <motion.div
-            id="frontend_layer"
-            className="relative z-30"
-            animate={{
-              translateY: selectedLayer === 0 ? 0 : 50,
-              scale: selectedLayer === 0 ? 1.4 : 1,
-              opacity: selectedLayer === 0 ? 1 : 0.3,
-            }}
-            whileHover={{
-              translateY: 0,
-              opacity: 1,
-              scale: 1.4,
-            }}
-            whileTap={{
-              scale: 1,
-            }}
-            onClick={() => handleManualSelection(0)}
-          >
-            <StackLayerComponent type="frontend" selected={selectedLayer} />
-          </motion.div>
-          <motion.div
-            id="backend_layer"
-            className="relative z-20"
-            animate={{
-              marginTop: selectedLayer === 1 ? 50 : 0,
-              marginBottom: selectedLayer === 1 ? 50 : 0,
-              scale: selectedLayer === 1 ? 1.4 : 1,
-              opacity: selectedLayer === 1 ? 1 : 0.3,
-            }}
-            whileHover={{
-              marginTop: 50,
-              marginBottom: 50,
-              opacity: 1,
-              scale: 1.4,
-            }}
-            whileTap={{
-              scale: 0.95,
-            }}
-            onClick={() => handleManualSelection(1)}
-          >
-            <StackLayerComponent type="backend" selected={selectedLayer} />
-          </motion.div>
-          <motion.div
-            id="tools_layer"
-            className="relative z-10"
-            animate={{
-              translateY: selectedLayer === 2 ? 0 : -50,
-              scale: selectedLayer === 2 ? 1.4 : 1,
-              opacity: selectedLayer === 2 ? 1 : 0.3,
-            }}
-            whileHover={{
-              translateY: 0,
-              opacity: 1,
-              scale: 1.4,
-            }}
-            whileTap={{
-              scale: 0.95,
-            }}
-            onClick={() => handleManualSelection(2)}
-          >
-            <StackLayerComponent selected={selectedLayer} />
-          </motion.div>
-        </div>
+      <div className="stack-vector-container ms-8">
         <motion.div
-          className="stack-description w-4/6 h-96 bg-gun-metal bg-opacity-50 rounded-lg relative"
+          id="frontend_layer"
+          className="relative z-30"
           animate={{
-            border: "2px solid gray",
-            borderColor:
-              selectedLayer === 0
-                ? "#9747ffcc"
-                : selectedLayer === 1
-                ? "#0d99ffcc"
-                : "#ff7c70cc",
-            boxShadow: `inset 0px 0px 80px -65px ${
-              selectedLayer === 0
-                ? "#9747FF"
-                : selectedLayer === 1
-                ? "#0d99ff"
-                : "#ff7c70"
-            }`,
+            translateY: selectedLayer === 0 ? 0 : 50,
+            scale: selectedLayer === 0 ? 1.4 : 1,
+            opacity: selectedLayer === 0 ? 1 : 0.3,
           }}
+          whileHover={{
+            translateY: 0,
+            opacity: 1,
+            scale: 1.4,
+          }}
+          whileTap={{
+            scale: 1,
+          }}
+          onClick={() => handleManualSelection(0)}
         >
-          <AnimatePresence>
-            {selectedLayer === 0 ? (
-              <>
-                <motion.h5
-                  key="frontend"
-                  className="absolute w-fit px-2 py-1 -top-8 left-0 right-0 mx-auto text-3xl text-snow-white font-rubik font-bold bg-[#9747FF] rounded-md drop-shadow-2xl uppercase text-center"
-                  initial={{
-                    y: -30,
-                    opacity: 0,
-                    scale: 0.6,
-                  }}
-                  animate={{
-                    y: 0,
-                    opacity: 1,
-                    scale: 1,
-                  }}
-                >
-                  Frontend
-                </motion.h5>
-                <FrontendStackComponent
-                  containerVariants={containerVariants}
-                  iconsVariants={iconsVariants}
-                />
-              </>
-            ) : selectedLayer === 1 ? (
-              <>
-                <motion.h5
-                  key="backend"
-                  className="absolute w-fit px-2 py-1 -top-8 left-0 right-0 mx-auto text-3xl text-snow-white font-rubik font-bold bg-[#0d99ff] rounded-md drop-shadow-2xl uppercase text-center"
-                  initial={{
-                    y: -30,
-                    opacity: 0,
-                    scale: 0.6,
-                  }}
-                  animate={{
-                    y: 0,
-                    opacity: 1,
-                    scale: 1,
-                  }}
-                >
-                  Backend
-                </motion.h5>
-                <BackendStackComponent
-                  containerVariants={containerVariants}
-                  iconsVariants={iconsVariants}
-                />
-              </>
-            ) : (
-              <>
-                <motion.h5
-                  key="tools"
-                  className="absolute w-fit px-2 py-1 -top-8 left-0 right-0 mx-auto text-3xl text-snow-white font-rubik font-bold bg-[#ff7c70] rounded-md drop-shadow-2xl uppercase text-center"
-                  initial={{
-                    y: -30,
-                    opacity: 0,
-                    scale: 0.6,
-                  }}
-                  animate={{
-                    y: 0,
-                    opacity: 1,
-                    scale: 1,
-                  }}
-                >
-                  TOOLS
-                </motion.h5>
-                <ToolsStackComponent
-                  containerVariants={containerVariants}
-                  iconsVariants={iconsVariants}
-                />
-              </>
-            )}
-          </AnimatePresence>
+          <StackLayerComponent type="frontend" selected={selectedLayer} />
+        </motion.div>
+        <motion.div
+          id="backend_layer"
+          className="relative z-20"
+          animate={{
+            marginTop: selectedLayer === 1 ? 50 : 0,
+            marginBottom: selectedLayer === 1 ? 50 : 0,
+            scale: selectedLayer === 1 ? 1.4 : 1,
+            opacity: selectedLayer === 1 ? 1 : 0.3,
+          }}
+          whileHover={{
+            marginTop: 50,
+            marginBottom: 50,
+            opacity: 1,
+            scale: 1.4,
+          }}
+          whileTap={{
+            scale: 0.95,
+          }}
+          onClick={() => handleManualSelection(1)}
+        >
+          <StackLayerComponent type="backend" selected={selectedLayer} />
+        </motion.div>
+        <motion.div
+          id="tools_layer"
+          className="relative z-10"
+          animate={{
+            translateY: selectedLayer === 2 ? 0 : -50,
+            scale: selectedLayer === 2 ? 1.4 : 1,
+            opacity: selectedLayer === 2 ? 1 : 0.3,
+          }}
+          whileHover={{
+            translateY: 0,
+            opacity: 1,
+            scale: 1.4,
+          }}
+          whileTap={{
+            scale: 0.95,
+          }}
+          onClick={() => handleManualSelection(2)}
+        >
+          <StackLayerComponent selected={selectedLayer} />
         </motion.div>
       </div>
+      <motion.div
+        className="stack-description w-4/6 h-96 bg-gun-metal bg-opacity-50 rounded-lg relative"
+        animate={{
+          border: "2px solid gray",
+          borderColor:
+            selectedLayer === 0
+              ? "#9747ffcc"
+              : selectedLayer === 1
+              ? "#0d99ffcc"
+              : "#ff7c70cc",
+          boxShadow: `inset 0px 0px 80px -65px ${
+            selectedLayer === 0
+              ? "#9747FF"
+              : selectedLayer === 1
+              ? "#0d99ff"
+              : "#ff7c70"
+          }`,
+        }}
+      >
+        <AnimatePresence>
+          {selectedLayer === 0 ? (
+            <>
+              <motion.h5
+                key="frontend"
+                className="absolute w-fit px-2 py-1 -top-8 left-0 right-0 mx-auto text-3xl text-snow-white font-rubik font-bold bg-[#9747FF] rounded-md drop-shadow-2xl uppercase text-center"
+                initial={{
+                  y: -30,
+                  opacity: 0,
+                  scale: 0.6,
+                }}
+                animate={{
+                  y: 0,
+                  opacity: 1,
+                  scale: 1,
+                }}
+              >
+                Frontend
+              </motion.h5>
+              <FrontendStackComponent
+                containerVariants={containerVariants}
+                iconsVariants={iconsVariants}
+              />
+            </>
+          ) : selectedLayer === 1 ? (
+            <>
+              <motion.h5
+                key="backend"
+                className="absolute w-fit px-2 py-1 -top-8 left-0 right-0 mx-auto text-3xl text-snow-white font-rubik font-bold bg-[#0d99ff] rounded-md drop-shadow-2xl uppercase text-center"
+                initial={{
+                  y: -30,
+                  opacity: 0,
+                  scale: 0.6,
+                }}
+                animate={{
+                  y: 0,
+                  opacity: 1,
+                  scale: 1,
+                }}
+              >
+                Backend
+              </motion.h5>
+              <BackendStackComponent
+                containerVariants={containerVariants}
+                iconsVariants={iconsVariants}
+              />
+            </>
+          ) : (
+            <>
+              <motion.h5
+                key="tools"
+                className="absolute w-fit px-2 py-1 -top-8 left-0 right-0 mx-auto text-3xl text-snow-white font-rubik font-bold bg-[#ff7c70] rounded-md drop-shadow-2xl uppercase text-center"
+                initial={{
+                  y: -30,
+                  opacity: 0,
+                  scale: 0.6,
+                }}
+                animate={{
+                  y: 0,
+                  opacity: 1,
+                  scale: 1,
+                }}
+              >
+                TOOLS
+              </motion.h5>
+              <ToolsStackComponent
+                containerVariants={containerVariants}
+                iconsVariants={iconsVariants}
+              />
+            </>
+          )}
+        </AnimatePresence>
+      </motion.div>
     </div>
   );
 }
